9e45ab4d5d6f29646e6cd3f942f4e1eae818e6ef
howlerdev506 <ppedroza558@gmail.com>
2024-06-20 10:41:38 -0600

    repo + ci/cd git 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1> SOS vending2</h1>
+      <h1> SOS vending1</h1>
     </main>
   );
 }
