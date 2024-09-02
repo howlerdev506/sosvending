1afaa01e4506907a6df621ce0316a4e5ff703d76
howlerdev506 <ppedroza558@gmail.com>
2024-09-02 13:20:10 -0600

    new pages 2

--- src/app/services/publicity/page.tsx
+++ src/app/services/publicity/page.tsx
@@ -6,159 +6,78 @@ import React from "react";
 const publicity = () => {
   return (
     <>
-      <section id="snacks" className="relative pt-6">
-        <div className="relative bg-clip-border text-gray-700 flex h-full min-h-[314px] w-full flex-col items-center justify-center">
-          <div className="container mx-auto text-center">
-            <h2 className="block antialiased tracking-normal  text-4xl font-semibold leading-[1.3] text-black-100 mb-4 mt-14">
+      <section id="snacks" className="relative pt-8  md:px-24">
+        <div className="bg-clip-border text-gray-700 flex h-full min-h-[314px] w-full items-center justify-center">
+          <div className="container mx-auto">
+            <h2 className="block antialiased tracking-normal text-4xl font-semibold leading-[1.3] text-black-100 mb-4 mt-14 text-center">
               Publicidad
             </h2>
-            <p className="block antialiased  text-xl font-normal leading-relaxed text-black-100 mb-8 opacity-70">
-              Ofrecemos una amplia gama de máquinas expendedoras de snacks y
-              bebidas para satisfacer las necesidades de cualquier negocio.
+            <p className="block antialiased  text-normal md:text-xl font-normal leading-relaxed text-black-100 opacity-70">
+
+            Nuestras máquinas expendedoras no solo son una fuente de conveniencia para los usuarios, sino que también ofrecen una excelente oportunidad para el uso como espacios publicitarios. Con pantallas digitales integradas y opciones de personalización, estas máquinas pueden exhibir anuncios, promociones y mensajes personalizados, capturando la atención de los consumidores en el momento justo. Esto permite maximizar el alcance de sus campañas publicitarias y promover productos o servicios de manera efectiva, convirtiendo cada máquina en un poderoso canal de comunicación y marketing para su negocio.
+              rendimiento.
             </p>
-          </div>
-        </div>
-        <div className="px-10 pt-8 pb-16 -mt-16 lg:px-30 xl:px-40">
-          <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4">
-            <Card vertical={true} />
-            <Card vertical={true} />
-            <Card vertical={true} />
-            <Card vertical={true} />
           </div>
         </div>
       </section>
 
       <div className="bg-gray-100 py-16">
         <div className="container mx-auto px-4 lg:px-8 flex flex-col lg:flex-row items-center">
-          <div className="lg:w-1/2 flex justify-center lg:justify-start">
+          <div className="lg:w-1/2 flex justify-center lg:justify-start" >
             <img
-              src="/path/to/your/laptop-image.png"
+              src="/images/vending-machine-publicity.webp"
               alt="Laptop showing application"
-              className="w-full max-w-md"
+              className="w-full max-w-md cover"
             />
           </div>
           <div className="lg:w-1/2 mt-8 lg:mt-0 lg:pl-12">
             <h2 className="text-3xl font-bold text-gray-800">
-              Secure. Trusted. Available.
+              Espacios publicitarios.
             </h2>
             <p className="mt-4 text-lg text-gray-600">
-              Secure your team’s communication with Wickr RAM’s multilayered,
-              256-bit Advanced Encryption Standard (AES) E2EE. Customers can
-              configure data retention in the cloud, or on premises. Additional
-              Wickr RAM benefits include:
+            Ofrecemos espacios publicitarios en nuestras máquinas expendedoras, brindando una excelente oportunidad para maximizar el alcance de su marca. Nuestro servicio incluye asesoramiento personalizado en el diseño de la publicidad y la ubicación estratégica de los anuncios, adaptándonos a las necesidades específicas de cada cliente.
             </p>
             <ul className="mt-6 space-y-4 text-gray-700">
               <li className="flex items-start">
                 <span className="text-yellow-500 mr-2">✔️</span>
                 <span>
-                  Available on AWS GovCloud (US) and Cloud One (via Carahsoft)
-                  and can support workloads up to Impact Level 5 (IL-5)
+                Las máquinas expendedoras están ubicadas en áreas de alto tráfico, lo que garantiza que los anuncios lleguen directamente a los consumidores en su rutina diaria.
                 </span>
               </li>
               <li className="flex items-start">
                 <span className="text-yellow-500 mr-2">✔️</span>
                 <span>
-                  Adversary-resilient communications as encryption keys are
-                  stored on local endpoints
+                Los usuarios interactúan regularmente con las máquinas, lo que aumenta la frecuencia con la que ven los anuncios y mejora el recuerdo de marca.
                 </span>
               </li>
               <li className="flex items-start">
                 <span className="text-yellow-500 mr-2">✔️</span>
                 <span>
-                  Denied, degraded, intermittent, and limited (DDIL) bandwidth
-                  capabilities—including disconnected, and out-of-band
-                  communications that can federate to the cloud when
-                  connectivity is restored
+                Con el uso de tecnología de telemetría, se pueden recopilar datos sobre la interacción con los anuncios
                 </span>
               </li>
-              <li className="flex items-start">
-                <span className="text-yellow-500 mr-2">✔️</span>
-                <span>
-                  Built using zero trust network design principles with a FIPS
-                  certified encryption library
-                </span>
-              </li>
-              <li className="flex items-start">
-                <span className="text-yellow-500 mr-2">✔️</span>
-                <span>
-                  Powerful integrations with open-source Java Node bots for
-                  interoperability
-                </span>
-              </li>
-              <li className="flex items-start">
-                <span className="text-yellow-500 mr-2">✔️</span>
-                <span>
-                  Multi-factor public key infrastructure (PKI) authentication
-                </span>
-              </li>
+              
             </ul>
           </div>
         </div>
       </div>
 
-      <div className="flex justify-center items-center mt-10 translate-x-6">
-        <AutoScroll>
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/8ee9f161-df19-4fa7-a2a6-edf9acf0e0d6?apiKey=7e8b177c7c374d8abaf3aebf27f1c17d&"
-            alt="Image 1"
-          />
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/80480f8a-69ad-4c30-88ba-f4e7ee08fc51?apiKey=7e8b177c7c374d8abaf3aebf27f1c17d&"
-            alt="Image 2"
-          />
-        </AutoScroll>
-      </div>
-
-      <div className="bg-gradient-to-b from-gray-900 to-black py-16">
-        <div className="container mx-auto px-4 lg:px-8 text-center">
-          <h2 className="text-4xl font-bold text-white">
-            Meet the <span className="text-purple-500">Innovators</span> Fueling
-            Nebula's Mission
-          </h2>
+      <section className="relative w-full h-80 bg-cover bg-center md:px-24 mt-4" style={{ backgroundImage: "url('/images/vending-hot-drinks.png')" }}>
+        <div className="relative bg-clip-border text-gray-700 flex h-full min-h-[314px] w-full  items-center justify-center">
+          <div className="w-1/2"></div>
+          <div className="text-black max-w-lg w-1/2 md:px-24">
+                <p className="md:text-sm lg:text-2xl font-light">
+                  Nos comprometemos a proporcionar un servicio integral que
+                  incluye la instalación, mantenimiento y reabastecimiento.
+                </p>
+                <p className="mt-4 font-bold text-lg lg:text-xl">
+                  + Cerca de ti
+                </p>
+                <p className="text-base lg:text-lg">SosVending</p>
+              </div>
         </div>
+      </section>
 
-        <div className="flex justify-center items-center mt-10 flex-shrink">
-          <AutoScroll>
-            <CardUser
-              name="Pablo Martinez"
-              title="Product Manager"
-              imageUrl="/images/vending-m.png"
-            />
-            <CardUser
-              name="Brigite Carderon"
-              title="Product Manager"
-              imageUrl="/images/vending-m.png"
-            />
-            <CardUser
-              name="Jose Pedroza"
-              title="Product Manager"
-              imageUrl="/images/vending-m.png"
-            />
-          </AutoScroll>
-        </div>
-      </div>
-
-      <div className="flex justify-center items-center mt-10 translate-x-6">
-        <AutoScroll>
-          <CardUser
-            name="Pablo Martinez"
-            title="Product Manager"
-            imageUrl="/images/vending-m.png"
-          />
-          <CardUser
-            name="Brigite Carderon"
-            title="Product Manager"
-            imageUrl="/images/vending-m.png"
-          />
-          <CardUser
-            name="Jose Pedroza"
-            title="Product Manager"
-            imageUrl="/images/vending-m.png"
-          />
-        </AutoScroll>
-      </div>
     </>
   );
 };
--- src/app/services/telemetry/page.tsx
+++ src/app/services/telemetry/page.tsx
@@ -1,66 +1,86 @@
+import { Card, UnderConstruction } from "@/components";
 
-import { Card, UnderConstruction } from '@/components'
-
-UnderConstruction
-import React from 'react'
+UnderConstruction;
+import React from "react";
 
 const telemetry = () => {
   return (
     <>
-    <section id="snacks" className="relative pt-6">
-      <div className="relative bg-clip-border text-gray-700 flex h-full min-h-[314px] w-full flex-col items-center justify-center">
-        <div className="container mx-auto text-center">
-          <h2 className="block antialiased tracking-normal  text-4xl font-semibold leading-[1.3] text-black-100 mb-4 mt-14">
-            Telemetria
-          </h2>
-          <p className="block antialiased  text-xl font-normal leading-relaxed text-black-100 mb-8 opacity-70">
-            Ofrecemos una amplia gama de máquinas expendedoras de snacks y
-            bebidas para satisfacer las necesidades de cualquier negocio.
-          </p>
+      <section id="snacks" className="relative pt-8  md:px-24">
+        <div className="bg-clip-border text-gray-700 flex h-full min-h-[314px] w-full items-center justify-center">
+          <div className="container mx-auto">
+            <h2 className="block antialiased tracking-normal text-4xl font-semibold leading-[1.3] text-black-100 mb-4 mt-14 text-center">
+              Telemetria
+            </h2>
+            <p className="block antialiased  text-normal md:text-xl font-normal leading-relaxed text-black-100 opacity-70">
+              Nuestras máquinas expendedoras no solo ofrecen una amplia gama de
+              productos, sino que también están equipadas con un avanzado
+              sistema de telemetría para facilitar la toma de decisiones de
+              negocio. Este servicio permite monitorear en tiempo real el
+              rendimiento de cada máquina, las ventas y el inventario,
+              proporcionando datos precisos y actualizados. De esta manera,
+              puede optimizar la reposición de productos, identificar tendencias
+              de consumo y mejorar la eficiencia operativa, asegurando que sus
+              máquinas estén siempre bien surtidas y funcionando al máximo
+              rendimiento.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="px-10 pt-8 pb-16 -mt-16 lg:px-30 xl:px-40">
-        <div className="grid gap-8 md:grid-cols-2 lg:grid-cols-4">        
-          <Card vertical={true}/>
-          <Card vertical={true}/>
-          <Card vertical={true}/>
-          <Card vertical={true}/>
-        </div>
-      </div>
-    </section>
+      </section>
 
-    
-    <div className="relative w-full overflow-hidden">
-      <svg
-        className="absolute top-0 left-0 w-full h-full"
-        viewBox="0 0 1440 320"
-        preserveAspectRatio="none"
-      >
-        <path
-          fill="#dce9f5"
-          fillOpacity="1"
-          d="M0,160L48,165.3C96,171,192,181,288,186.7C384,192,480,192,576,186.7C672,181,768,171,864,181.3C960,192,1056,224,1152,208C1248,192,1344,128,1392,96L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-        ></path>
-      </svg>
+      <section>
+        <div className="relative w-full overflow-hidden">
+          <svg
+            className="absolute top-0 left-0 w-full h-full"
+            viewBox="0 0 1440 320"
+            preserveAspectRatio="none"
+          >
+            <path
+              fill="#dce9f5"
+              fillOpacity="1"
+              d="M0,160L48,165.3C96,171,192,181,288,186.7C384,192,480,192,576,186.7C672,181,768,171,864,181.3C960,192,1056,224,1152,208C1248,192,1344,128,1392,96L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            ></path>
+          </svg>
 
-      <div className="relative flex justify-around items-center h-64">
-        <div className="text-center bg-white rounded-full shadow-lg w-48 h-48 flex flex-col justify-center items-center">
-          <div className="text-gray-600 font-semibold">ROI AFTER 1 YEAR</div>
-          <div className="text-6xl text-green-600 font-bold">172%</div>
+          <div className="relative flex justify-around items-center h-64">
+            <div className="text-center bg-white rounded-full shadow-lg w-48 h-48 flex flex-col justify-center items-center">
+              <div className="text-gray-600 font-semibold">
+                control de inventario
+              </div>
+              <div className="text-2xl text-green-600 font-bold">Optimizado</div>
+            </div>
+            <div className="text-center bg-white rounded-full shadow-lg w-48 h-48 flex flex-col justify-center items-center">
+              <div className="text-gray-600 font-semibold">Menjo de productos</div>
+              <div className="text-2xl text-green-600 font-bold">Preferencial </div>
+            </div>
+            <div className="text-center bg-white rounded-full shadow-lg w-48 h-48 flex flex-col justify-center items-center">
+              <div className="text-gray-600 font-semibold">
+                Timepo de respuesta
+              </div>
+              <div className="text-2xl text-green-600 font-bold">inmediato</div>
+            </div>
+          </div>
         </div>
-        <div className="text-center bg-white rounded-full shadow-lg w-48 h-48 flex flex-col justify-center items-center">
-          <div className="text-gray-600 font-semibold">LESS DOWNTIME</div>
-          <div className="text-6xl text-green-600 font-bold">80%</div>
+      </section>
+
+      <section className="relative w-full h-80 bg-cover bg-center md:px-24 mt-2" style={{ backgroundImage: "url('/images/vending-hot-drinks.png')" }}>
+        <div className="relative bg-clip-border text-gray-700 flex h-full min-h-[314px] w-full  items-center justify-center">
+          <div className="w-1/2"></div>
+          <div className="text-black max-w-lg w-1/2 md:px-24">
+                <p className="md:text-sm lg:text-2xl font-light">
+                  Nos comprometemos a proporcionar un servicio integral que
+                  incluye la instalación, mantenimiento y reabastecimiento.
+                </p>
+                <p className="mt-4 font-bold text-lg lg:text-xl">
+                  + Cerca de ti
+                </p>
+                <p className="text-base lg:text-lg">SosVending</p>
+              </div>
         </div>
-        <div className="text-center bg-white rounded-full shadow-lg w-48 h-48 flex flex-col justify-center items-center">
-          <div className="text-gray-600 font-semibold">LESS UNNECESSARY VISITS</div>
-          <div className="text-6xl text-green-600 font-bold">65%</div>
-        </div>
-      </div>
-    </div>
-    
+      </section>
+
     </>
-  )
-}
+  );
+};
 
-export default telemetry
+export default telemetry;
